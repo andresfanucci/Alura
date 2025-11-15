ab71b6b3f9495e8868fa9e55985711f1771689a0
andresfanucci <andresfanucci@gmail.com>
2025-11-14 23:31:51 -0300

    Add new project and implement Musica class functionality
    Added `Desafio_1.csproj` with .NET configuration for `net10.0`.
    Updated `ScreenSound.slnx` to include the new project.
    Created `Musica` class with attributes and a method to display
    song details. Updated `Program.cs` to instantiate and display
    details of two `Musica` objects.

---
+++ 2_OrientacaoObjetos/ScreenSound/ScreenSound/Musica.cs
@@ -0,0 +1,18 @@
+﻿internal class Musica
+{
+    //Atributos
+    public string nome;
+
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    //Métodos
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome da Música: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração(segundos): {duracao}");
+        Console.WriteLine($"Disponível: {(disponivel ? "Disponível no Plano" : "Adquira o plano Plus+")}");
+    }
+}
--- 2_OrientacaoObjetos/ScreenSound/ScreenSound/Program.cs
+++ 2_OrientacaoObjetos/ScreenSound/ScreenSound/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Musica musica1 = new Musica();
+
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+musica1.duracao = 230;
+musica1.disponivel = true;
+musica1.ExibirFichaTecnica();
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+musica2.duracao = 210;
+musica2.disponivel = false;
+musica2.ExibirFichaTecnica();
